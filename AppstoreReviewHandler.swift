3973c1d88db22280b37e57c7c30d10ce1a02f0fb
Markus Wilhelm <wilhelm@mw-systems.com>
2018-10-22 18:12:51 +0200

    add AppstoreRequestHandler

---
+++ AppstoreReviewHandler.swift
@@ -0,0 +1,47 @@
+//
+//  AppstoreReviewHandler.swift
+//  Cocteles
+//
+//  Created by Markus Wilhelm on 21.10.18.
+//  Copyright © 2018 Markus Wilhelm. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+class AppstoreReviewHandler: NSObject {
+    
+    private let MINIMUM_APP_LAUNCHES_UNTIS_FIRST_REQUEST = 3
+    private let REVIEW_SESSION_COUNT_KEY = "app_sessions_count_key"
+    
+    private let defaults = UserDefaults.standard
+    
+    override init() {
+        super.init()
+    }
+    
+    public func recordAppLaunch() {
+        var appLaunches = defaults.integer(forKey: REVIEW_SESSION_COUNT_KEY)
+        appLaunches += 1
+        defaults.set(appLaunches, forKey: REVIEW_SESSION_COUNT_KEY)
+        defaults.synchronize()
+        //TODO - Add Analytics event ?!
+    }
+    
+    public func tryToGetAppstoreReview() {
+        let appLaunches = defaults.integer(forKey: REVIEW_SESSION_COUNT_KEY)
+        if  appLaunches >= MINIMUM_APP_LAUNCHES_UNTIS_FIRST_REQUEST {
+            self.askForReview()
+            //TODO - Add Analytics event ?!
+        }
+    }
+    
+    private func askForReview() {
+        if #available( iOS 10.3,*) {
+            // Note that this is not shown every time you want it.
+            // Apple handles it in their own way so that a user will see it max three times a year.
+            SKStoreReviewController.requestReview()
+            //TODO - Add Analytics event ?!
+        }
+    }
+}
